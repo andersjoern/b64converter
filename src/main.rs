6d241764d95e59f63833288d2b7a12e7cfc577e5
Anders J. Andersen <andersj.andersen@gmail.com>
2021-02-21 13:03:38 +0100

    Initial commit of b64converter

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use fltk::{app::*, button::*, text::*, window::*, input::*, dialog::*};
+use base64::{encode, decode};
+
+fn main() {
+    let app = App::default().with_scheme(AppScheme::Gtk);
+    let mut wind = Window::default()
+        .with_size(600, 380)
+        .center_screen()
+        .with_label("Encode - Decode Base64 strings");
+    let _ = TextDisplay::new(10, 40, 580, 40, "Normal text");
+    let normal_text = MultilineInput::new(10, 40, 580, 100, "");
+    let _ = TextDisplay::new(10, 180, 580, 40, "Base64 text");
+    let base64_text = MultilineInput::new(10, 180, 580, 100, "");
+
+    let mut button_encode = Button::new(220, 310, 80, 40, "Encode");
+    let mut button_decode = Button::new(310, 310, 80, 40, "Decode");
+
+    wind.end();
+    wind.show();
+
+    let work_normal_text = normal_text.clone();
+    let work_base64_text = base64_text.clone();
+    button_encode.set_callback(Box::new(move || {
+        let text_normal = work_normal_text.value();
+        work_base64_text.set_value(&encode(text_normal));
+    }));
+
+    button_decode.set_callback(Box::new(move || {
+        let text_base64 = base64_text.value();
+        let decoded_text = match decode(text_base64) {
+            Ok(dc) => String::from_utf8(dc).unwrap(),
+            Err(e) => {                
+                message(100, 100, ("Error: ".to_string() + &e.to_string()).as_str()); 
+                "".to_string()
+            }
+        };
+        normal_text.set_value(&decoded_text);
+    }));
+
+    app.run().unwrap();
+}
