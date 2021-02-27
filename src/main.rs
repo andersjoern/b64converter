cc75b966aa0e101f04c8363d1e35b3caa5207e70
Anders J. Andersen <andersj.andersen@gmail.com>
2021-02-27 17:31:44 +0100

    ..

--- src/main.rs
+++ src/main.rs
@@ -31,12 +31,10 @@ fn main() {
             Ok(dc) => {
                 match String::from_utf8(dc) {
                     Ok(s) => normal_text.set_value(&s),
-                    Err(e) => {alert_default(("Error: ".to_string() + &e.to_string()).as_str());}
+                    Err(e) => alert_default(("Error: ".to_string() + &e.to_string()).as_str()),
                 }
             },
-            Err(e) => {                
-                alert_default(("Error: ".to_string() + &e.to_string()).as_str()); 
-            }
+            Err(e) => alert_default(("Error: ".to_string() + &e.to_string()).as_str()),
         };
     }));
 
